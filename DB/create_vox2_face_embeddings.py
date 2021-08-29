2eaaf79eabdf1ed42f1b7b7b3fed4d3f6214dbb9
松语 <1796296884@qq.com>
2021-08-29 10:10:33 +0000

    no no

---
+++ DB/create_vox2_face_embeddings.py
@@ -0,0 +1,93 @@
+# -*-coding: utf-8 -*-
+"""
+    # 该文件用于生成embedding数据库（人脸特征），后面待检测识别的人脸，只需要与这些embedding数据库（人脸特征）进行相似性比较，就可以识别人脸啦！！！！
+"""
+import time
+import datetime
+import numpy as np
+from utils import image_processing , file_processing, debug
+import face_recognition
+import os
+import cv2
+
+resize_width = 160
+resize_height = 160
+
+### 获得face_embedding
+def get_face_embedding(model_path, names_list, filePath_list):
+    # 转换颜色空间RGB or BGR
+    colorSpace = "RGB"
+    # 初始化mtcnn人脸检测
+    face_detect = face_recognition.Facedetection()
+    # 初始化facenet
+    face_net = face_recognition.facenetEmbedding(model_path) # model_path = 'models/20180408-102900' # faceNet模型路径；保存有FaceNet的Pretrained Model
+
+    embeddings = [] # 用于保存人脸特征的数据库
+    image_name_list = [] # 保存人脸的name，与embeddings一一对应
+    image_path_list = [] # 保存图像的路径,本工作会将其作为图片的label
+    print("开始处理图片...")
+    for  image_path in filePath_list:
+        print("processing image：{}".format(image_path))    
+        # processing image：DB/Vox2_Face/vox2_test_mp4/mp4/id00017/01dfn2spqyE/001_0050.jpg
+        image = image_processing.read_image_gbk(image_path, colorSpace=colorSpace)
+        # 进行人脸检测，获得bounding_box
+        bboxes, landmarks = face_detect.detect_face(image)
+        bboxes, landmarks = face_detect.get_square_bboxes(bboxes, landmarks, fixed="height")
+        # image_processing.show_image_boxes("image",image, bboxes)
+        # 下面两种情况生成不了人脸嵌入，程序会选择跳过
+        if bboxes == [] or landmarks == []:
+            print("--- {} image have no face !".format(image_path))
+            continue    # 注意，由于跳过，会影响嵌入生成的数量少于实际的图片数
+        if len(bboxes) >= 2 or len(landmarks) >= 2:
+            print("--- {} image have {} faces ".format(image_path, len(bboxes)))  # 只有一张人脸代表正常，不会print提示
+            continue
+        # 获得人脸区域
+        face_images = image_processing.get_bboxes_image(image, bboxes, resize_height, resize_width)
+        # 人脸预处理，归一化
+        face_images = image_processing.get_prewhiten_images(face_images, normalization=True)
+        # 利用face_net获得人脸特征embeddings
+        pred_emb = face_net.get_embedding(face_images)
+        # 保存embeddings、label_list、 image_path_list
+        # 可以选择保存image_list或者names_list作为人脸的标签  # 测试时建议保存image_list，这样方便知道被检测人脸与哪一张图片相似
+        embeddings.append(pred_emb) 
+        image_path_list.append(image_path[-31:])  # 这里自己加的,注意要根据情况修改里面的数字  -31 爲 id00017/01dfn2spqyE/001_050.jpg
+    print("embeddings、image_path_list已保存成功\n")
+    return embeddings, image_path_list
+
+def create_face_embedding(model_path, dataset_path, out_face_emb_path, out_face_label_path):
+    # 可以获取目录dataset_path文件夹下所有文件，包括子目录下的所有文件路径（files_list），其中names_list就是子目录的文件名，一般子目录作为样本的标签。
+    filePath_list, names_list = file_processing.gen_files_labels(dataset_path, postfix=['*.jpg']) # return filePath_list, name_list
+    embeddings, image_path_list = get_face_embedding(model_path, names_list, filePath_list)
+    # print("image_path_list:{}".format(image_path_list))       # label ，这里是图片的path ['id11001/WnFx5eZVVic/0004100.jpg', 'id11001/ZKP0nYyjg5s/0001050.jpg', ..., 'id11001/ZKP0nYyjg5s/0001150.jpg',]
+    print("The Face datasets have {} label\n".format(len(image_path_list)))  # 即拥有相应数量的用户
+            # The Vox1 test set have 4826 label, Vox1 dev set have 149519 label.
+            # The Vox2 test set have 126665 label, Vox2 dev set have 149519 label.
+
+    # 保存 embeddings,image_path_list
+    embeddings = np.asarray(embeddings)       # <class 'numpy.ndarray'>
+    print('embeddings的长度{}'.format(len(embeddings)))
+    np.save(out_face_emb_path, embeddings)    # 将embeddings 保存为.npy文件,,
+    # 利用file_processing.write_list_data()函数 保存list[]的数据到txt文件，每个元素分行
+    file_processing.write_list_data(out_face_label_path, image_path_list, mode='w')   # 将用户的label（名字/id）保存为txt文件
+
+
+if __name__ == '__main__':
+    model_path = 'DB/models/20180408-102900/'           # faceNet模型路径；保存有FaceNet的Pretrained Model
+    in_dev_dataset_path = 'DB/Vox2_Face/vox2_dev_mp4/mp4'       # 人脸数据库路径，每一类单独一个文件夹
+    out_dev_face_emb_path = 'DB/Vox2_Face/emb/vox2_face_D_embedding.npy'  # 输出embeddings的保存路径；.npy是原始数据集经Embedding后的输出结果
+    out_dev_face_label_path = 'DB/Vox2_Face/emb/vox2_face_D_label.txt'  # 输出与embeddings一一对应的path标签
+    
+    in_test_dataset_path = 'DB/Vox2_Face/vox2_test_mp4/mp4'       
+    out_test_face_emb_path = 'DB/Vox2_Face/emb/vox2_face_T_embedding.npy'  
+    out_test_face_label_path = 'DB/Vox2_Face/emb/vox2_face_T_label.txt'  
+    # 计算程序的处理时间
+    start_date = datetime.datetime.now()
+    print("开始处理时间为：{}".format(str(start_date)))
+    ## 创建人脸embedding
+    # create_face_embedding(model_path, in_dev_dataset_path, out_dev_face_emb_path, out_dev_face_label_path)
+    create_face_embedding(model_path, in_test_dataset_path, out_test_face_emb_path, out_test_face_label_path)
+
+    end_date = datetime.datetime.now()
+    print("数据处理总时长为：{}".format(str(start_date - end_date)))
+
+
---
+++ DB/face_recognition.py
@@ -0,0 +1,119 @@
+# -*-coding: utf-8 -*-
+"""
+    @Project: faceRecognition
+    @File   : face_recognition.py
+    @Author : panjq
+    @E-mail : pan_jinquan@163.com
+    @Date   : 2018-12-07 11:33:30
+"""
+import facenet
+import tensorflow as tf
+import numpy as np
+import align.detect_face as detect_face
+# align：这个文件夹是从facenet中拷贝的，https://github.com/davidsandberg/facenet/tree/master/src/align，主要是MTCNN人脸检测的相关文件。
+
+# 利用Facenet模型生成嵌入向量
+class facenetEmbedding:
+    def __init__(self,model_path):
+        self.sess = tf.InteractiveSession()
+        self.sess.run(tf.global_variables_initializer())
+        # Load the model
+        facenet.load_model(model_path)
+        # Get input and output tensors
+        self.images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+        self.tf_embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+        self.phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+
+    def get_embedding(self,images):
+        feed_dict = {self.images_placeholder: images, self.phase_train_placeholder: False}
+        embedding = self.sess.run(self.tf_embeddings, feed_dict=feed_dict)
+        return embedding
+
+    def free(self):
+        self.sess.close()
+
+# 利用MTCNN模型进行人脸检测
+class Facedetection:
+    def __init__(self):
+        self.minsize = 30  # minimum size of face
+        self.threshold = [0.6, 0.7, 0.7]  # three steps's threshold
+        self.factor = 0.709  # scale factor
+        print('Creating networks and loading parameters')
+        with tf.Graph().as_default():
+            # gpu_memory_fraction = 1.0
+            # gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_memory_fraction)
+            # sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+            sess = tf.Session()
+            with sess.as_default():
+                model_path = 'models/20180408-102900' # 我加的
+                self.pnet, self.rnet, self.onet = detect_face.create_mtcnn(sess, None)
+
+
+    def detect_face(self,image,fixed=None):
+        '''
+        mtcnn人脸检测，
+        PS：人脸检测获得bboxes并不一定是正方形的矩形框，参数fixed指定等宽或者等高的bboxes
+        :param image:
+        :param fixed:
+        :return:
+        '''
+        bboxes, landmarks = detect_face.detect_face(image, self.minsize, self.pnet, self.rnet, self.onet, self.threshold, self.factor)
+        landmarks_list = []
+        landmarks=np.transpose(landmarks)
+        bboxes=bboxes.astype(int)
+        bboxes = [b[:4] for b in bboxes]
+        for landmark in landmarks:
+            face_landmarks = [[landmark[j], landmark[j + 5]] for j in range(5)]
+            landmarks_list.append(face_landmarks)
+        if fixed is not None:
+            bboxes,landmarks_list=self.get_square_bboxes(bboxes, landmarks_list, fixed)
+        return bboxes,landmarks_list
+
+    def get_square_bboxes(self, bboxes, landmarks, fixed="height"):
+        '''
+        获得等宽或者等高的bboxes
+        :param bboxes:
+        :param landmarks:
+        :param fixed: width or height
+        :return:
+        '''
+        new_bboxes = []
+        for bbox in bboxes:
+            x1, y1, x2, y2 = bbox
+            w = x2 - x1
+            h = y2 - y1
+            center_x, center_y = (int((x1 + x2) / 2), int((y1 + y2) / 2))
+            if fixed == "height":
+                dd = h / 2
+            elif fixed == 'width':
+                dd = w / 2
+            x11 = int(center_x - dd)
+            y11 = int(center_y - dd)
+            x22 = int(center_x + dd)
+            y22 = int(center_y + dd)
+            new_bbox = (x11, y11, x22, y22)
+            new_bboxes.append(new_bbox)
+        return new_bboxes, landmarks
+
+
+# def detection_face(img):
+#     minsize = 20  # minimum size of face
+#     threshold = [0.6, 0.7, 0.7]  # three steps's threshold
+#     factor = 0.709  # scale factor
+#     print('Creating networks and loading parameters')
+#     with tf.Graph().as_default():
+#         # gpu_memory_fraction = 1.0
+#         # gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_memory_fraction)
+#         # sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+#         sess = tf.Session()
+#         with sess.as_default():
+#             pnet, rnet, onet = detect_face.create_mtcnn(sess, None)
+#             bboxes, landmarks = detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+#     landmarks = np.transpose(landmarks)
+#     bboxes = bboxes.astype(int)
+#     bboxes = [b[:4] for b in bboxes]
+#     landmarks_list=[]
+#     for landmark in landmarks:
+#         face_landmarks = [[landmark[j], landmark[j + 5]] for j in range(5)]
+#         landmarks_list.append(face_landmarks)
+#     return bboxes,landmarks_list
